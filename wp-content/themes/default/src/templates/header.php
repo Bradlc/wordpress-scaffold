aadd140e494b0e49770045ac41a36b9c852e1541
Brad Cornes <bradlc41@gmail.com>
2015-09-15 21:12:29 +0100

    Update header
    Add page title and remove no-js class on body

--- wp-content/themes/default/src/templates/header.php
+++ wp-content/themes/default/src/templates/header.php
@@ -1,9 +1,15 @@
 <!doctype html>
-<html class="no-js" lang="en-GB">
+<html lang="en-GB">
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<title></title>
+		<?php
+		$pageTitle = wp_title( '', false );
+		$pageTitle .= ( wp_title( '', false ) ) ? ' : ' : '';
+		$pageTitle .= get_bloginfo( 'name' );
+		$pageTitle = trim( $pageTitle );
+		?>
+		<title><?=$pageTitle?></title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
