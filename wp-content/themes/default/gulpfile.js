de6de41763293f0fb8e43fbeca88c53ef4488fb6
Brad Cornes <bradlc41@gmail.com>
2015-11-14 23:26:14 +0000

    Update code style in gulpfile

--- wp-content/themes/default/gulpfile.js
+++ wp-content/themes/default/gulpfile.js
@@ -1,98 +1,98 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    plumber = require('gulp-plumber'),
-    notify = require('gulp-notify'),
-    concat = require('gulp-concat'),
-    replace = require('gulp-replace'),
-    rename = require('gulp-rename'),
-    stylus = require('gulp-stylus'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifyCss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    sourcemaps = require('gulp-sourcemaps'),
-    imagemin = require('gulp-imagemin'),
-    watch = require('gulp-watch'),
-    rev = require('gulp-rev'),
-    revReplace = require('gulp-rev-replace'),
-    cssRef = require('gulp-rev-css-url'),
-    reference = require('gulp-reference'),
+var gulp = require( 'gulp' );
+var gutil = require( 'gulp-util' );
+var plumber = require( 'gulp-plumber' );
+var notify = require( 'gulp-notify' );
+var concat = require( 'gulp-concat' );
+var replace = require( 'gulp-replace' );
+var rename = require( 'gulp-rename' );
+var stylus = require( 'gulp-stylus' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+var minifyCss = require( 'gulp-minify-css' );
+var uglify = require( 'gulp-uglify' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+var imagemin = require( 'gulp-imagemin' );
+var watch = require( 'gulp-watch' );
+var rev = require( 'gulp-rev' );
+var revReplace = require( 'gulp-rev-replace' );
+var cssRef = require( 'gulp-rev-css-url' );
+var reference = require( 'gulp-reference' );
 
-    fs = require('fs'),
-    del = require('del'),
-    through = require('through2'),
-    path = require('path'),
-    spawn = require('child_process').spawn,
+var fs = require( 'fs' );
+var del = require( 'del' );
+var through = require( 'through2' );
+var path = require( 'path' );
+var spawn = require( 'child_process' ).spawn;
 
-    vinylPaths = require('vinyl-paths'),
+var vinylPaths = require( 'vinyl-paths' );
 
-    livereload = require('gulp-livereload'),
+var livereload = require( 'gulp-livereload' );
 
-    webpack = require('webpack');
+var webpack = require( 'webpack' );
 
 /*----------------------------*\
 	Load options from package file
 \*----------------------------*/
-var pkg = require('./package.json');
+var pkg = require( './package.json' );
 
 /*----------------------------*\
 	Clean
 \*----------------------------*/
-gulp.task('clean_css', function(cb){
-	del(['./assets/css/*'], cb);
-});
-gulp.task('clean_js', function(cb){
-	del(['./assets/js/*'], cb);
-});
-gulp.task('clean_all', function(cb){
-	del(['./assets/**/*'], cb);
-});
+gulp.task( 'clean_css', function( cb ) {
+	del( ['./assets/css/*'], cb );
+} );
+gulp.task( 'clean_js', function( cb ) {
+	del( ['./assets/js/*'], cb );
+} );
+gulp.task( 'clean_all', function( cb ) {
+	del( ['./assets/**/*'], cb );
+} );
 
-gulp.task('unrev', function(cb){
+gulp.task( 'unrev', function( cb ) {
 	var vp = vinylPaths();
-	gulp.src('./assets/**/*.{css,js,png,jpg,jpeg,gif,webp,svg,ico,eot,ttf,woff,woff2}')
-		.pipe(vp)
-		.pipe(rename(function(path){
-			path.basename = path.basename.replace(/-[a-zA-Z0-9]{8,10}$/, '');
-		}))
-		.pipe(gulp.dest('./assets'))
-		.on('end', function(){
-			if(vp.paths){
-            	del(vp.paths, cb);
-            }
-        });
-});
+	gulp.src( './assets/**/*.{css,js,png,jpg,jpeg,gif,webp,svg,ico,eot,ttf,woff,woff2}' )
+	.pipe( vp )
+	.pipe( rename( function( path ) {
+		path.basename = path.basename.replace( /-[a-zA-Z0-9]{8,10}$/, '' );
+	} ) )
+	.pipe( gulp.dest( './assets' ) )
+	.on( 'end', function() {
+		if( vp.paths ) {
+			del( vp.paths, cb );
+		}
+	} );
+} );
 
 /*----------------------------*\
 	Compile Stylus
 \*----------------------------*/
-gulp.task('css', ['clean_css'], function(){
-	return gulp.src('./src/styl/main.styl')
-		.pipe(plumber({
-			errorHandler: notify.onError({
-				title: 'CSS Error',
-				message: '<%= error.message %>',
-				icon: 'http://littleblackboxdev.co.uk/gulp-logo.png'
-			})
-		}))
-		.pipe(stylus({compress:false, url:'embedurl'}))
-		.pipe(autoprefixer())
-		.pipe(minifyCss())
-		.pipe(gulp.dest('./assets/css'));
-});
+gulp.task( 'css', ['clean_css'], function() {
+	return gulp.src( './src/styl/main.styl' )
+	.pipe( plumber( {
+		errorHandler: notify.onError( {
+			title: 'CSS Error',
+			message: '<%= error.message %>',
+			icon: 'http://littleblackboxdev.co.uk/gulp-logo.png'
+		} )
+	} ) )
+	.pipe( stylus( {compress: false, url: 'embedurl'} ) )
+	.pipe( autoprefixer() )
+	.pipe( minifyCss() )
+	.pipe( gulp.dest( './assets/css' ) );
+} );
 
 /*----------------------------*\
 	Optimize images
 \*----------------------------*/
-gulp.task('images', function(){
-	return gulp.src('./src/images/**')
-		.pipe(imagemin({progressive:true}))
-		.pipe(gulp.dest('./assets/images'));
-});
+gulp.task( 'images', function() {
+	return gulp.src( './src/images/**' )
+	.pipe( imagemin( {progressive: true} ) )
+	.pipe( gulp.dest( './assets/images' ) );
+} );
 
 /*----------------------------*\
 	JavaScript
 \*----------------------------*/
-gulp.task( 'webpack', ['clean_js'], function( callback ) {
+gulp.task( 'webpack', ['clean_js'], function( cb ) {
 	// run webpack
 	webpack( {
 		context: __dirname + '/src/js',
@@ -118,149 +118,136 @@ gulp.task( 'webpack', ['clean_js'], function( callback ) {
 		gutil.log( '[webpack]', stats.toString( {
 			colors: true
 		} ) );
-		callback();
+		cb();
 	} );
 } );
 
-if(pkg.es2015) {
+if( pkg.es2015 ) {
 
-	gulp.task('js', ['webpack'], function(){
-		return gulp.src('assets/js/main.js')
-			.pipe(uglify())
-			.pipe(gulp.dest('assets/js'));
-	});
+	gulp.task( 'js', ['webpack'], function() {
+		return gulp.src( 'assets/js/main.js' )
+		.pipe( uglify() )
+		.pipe( gulp.dest( 'assets/js' ) );
+	} );
 
 } else {
 
-	gulp.task('js', ['clean_js'], function(){
-		return gulp.src('./src/js/main.js')
-			.pipe(plumber({
-				errorHandler: notify.onError({
-					title: 'JavaScript Error',
-					message: '<%= error.message %>',
-					icon: 'http://littleblackboxdev.co.uk/gulp-logo.png'
-				})
-			}))
-			.pipe(sourcemaps.init())
-			.pipe(reference())
-			.pipe(uglify())
-			.pipe(sourcemaps.write('.', {sourceRoot:'./src/js'}))
-			.pipe(gulp.dest('./assets/js'));
-	});
+	gulp.task( 'js', ['clean_js'], function() {
+		return gulp.src( './src/js/main.js' )
+		.pipe( plumber( {
+			errorHandler: notify.onError( {
+				title: 'JavaScript Error',
+				message: '<%= error.message %>',
+				icon: 'http://littleblackboxdev.co.uk/gulp-logo.png'
+			} )
+		} ) )
+		.pipe( sourcemaps.init() )
+		.pipe( reference() )
+		.pipe( uglify() )
+		.pipe( sourcemaps.write( '.', {sourceRoot: './src/js'} ) )
+		.pipe( gulp.dest( './assets/js' ) );
+	} );
 
 }
 
-gulp.task('copy_fonts', function(){
-	return gulp.src('./src/fonts/*')
-		.pipe(gulp.dest('./assets/fonts'));
-});
-gulp.task('copy_templates', function(){
-	return gulp.src('./src/templates/*')
-		.pipe(gulp.dest('.'));
-});
+gulp.task( 'copy_fonts', function() {
+	return gulp.src( './src/fonts/*' )
+	.pipe( gulp.dest('./assets/fonts' ) );
+} );
+gulp.task( 'copy_templates', function() {
+	return gulp.src( './src/templates/*' )
+	.pipe( gulp.dest( '.' ) );
+} );
 
 /*----------------------------*\
 	Prefix assets with Wordpress template directory
 \*----------------------------*/
-gulp.task('replace_wp', function(){
-	return gulp.src('./*.php')
-		.pipe(replace(/(["'])assets\//g, '$1<?=get_template_directory_uri()?>/assets/'))
-		.pipe(gulp.dest('.'))
-		.on('end', function(){
-			livereload.reload();
-		});
-});
+gulp.task( 'replace_wp', function() {
+	return gulp.src( './*.php' )
+	.pipe( replace( /(["'])assets\//g, '$1<?=get_template_directory_uri()?>/assets/' ) )
+	.pipe( gulp.dest( '.' ) )
+	.on( 'end', function() {
+		livereload.reload();
+	} );
+} );
 
 /*----------------------------*\
 	File revisioning
 \*----------------------------*/
 // Remove originals
 var rmOrig = function() {
-  return through.obj(function(file, enc, cb) {
-
-    if (file.revOrigPath) {
-      fs.unlink(file.revOrigPath, function(err) {
-      });
-    }
-
-    this.push(file);
-    return cb();
-  });
+	return through.obj( function( file, enc, cb ) {
+		if( file.revOrigPath ) {
+			fs.unlink( file.revOrigPath, function( err ) {
+			} );
+		}
+		this.push( file );
+		return cb();
+	} );
 };
 
 // Save revisioned files, removing originals
-gulp.task('revision', function(){
-	return gulp.src(pkg.revision, {base: path.join(process.cwd(), 'assets')})
-    .pipe(rev())
-    .pipe(cssRef()) // replace references in CSS
-    .pipe(gulp.dest('./assets'))
-    .pipe(rmOrig()) // remove originals
-    .pipe(rev.manifest({merge:true})) // save manifest
-    .pipe(gulp.dest('./assets'));
-});
+gulp.task( 'revision', function() {
+	return gulp.src( pkg.revision, {base: path.join( process.cwd(), 'assets' ) } )
+	.pipe( rev() )
+	.pipe( cssRef() ) // replace references in CSS
+	.pipe( gulp.dest( './assets' ) )
+	.pipe( rmOrig() ) // remove originals
+	.pipe( rev.manifest( {merge: true} ) ) // save manifest
+	.pipe( gulp.dest( './assets' ) );
+} );
 
 // Replace references to files
-gulp.task('rev', ['revision'], function(){
-	var manifest = gulp.src('./assets/rev-manifest.json');
+gulp.task( 'rev', ['revision'], function() {
+	var manifest = gulp.src( './assets/rev-manifest.json' );
 
-	return gulp.src('./*.php')
-		.pipe(revReplace({
-			manifest: manifest,
-			replaceInExtensions: ['.php']
-		}))
-		.pipe(gulp.dest('.'));
-});
+	return gulp.src( './*.php' )
+	.pipe( revReplace( {
+		manifest: manifest,
+		replaceInExtensions: ['.php']
+	} ) )
+	.pipe( gulp.dest( '.' ) );
+} );
 
 /*----------------------------*\
 	File watcher
 \*----------------------------*/
-gulp.task('default', ['cleanbuild'], function(){
-	gulp.start('watch');
-});
-gulp.task('watch', function(){
+gulp.task( 'default', ['cleanbuild'], function() {
+	gulp.start( 'watch' );
+} );
+gulp.task( 'watch', function() {
 	livereload.listen();
-	watch(['./src/styl/**/*', './src/js/**/*', './src/fonts/**/*', './src/templates/**/*'], function(){
-		gulp.start('master:notimages');
-	});
-	watch('./src/images/**/*', function(){
-		gulp.start('master');
-	});
-});
+	watch( ['./src/styl/**/*', './src/js/**/*', './src/fonts/**/*', './src/templates/**/*'], function() {
+		gulp.start( 'master:notimages' );
+	} );
+	watch( './src/images/**/*', function() {
+		gulp.start( 'master' );
+	} );
+} );
 
 
 
 
-gulp.task('master', ['unrev'], function(){
-	gulp.start('build');
-});
+gulp.task( 'master', ['unrev'], function() {
+	gulp.start( 'build' );
+} );
 
-gulp.task('build', [
-	'images',
-	'copy_templates',
-	'copy_fonts',
-	'css',
-	'js'
-], function(){
-	gulp.start('build2');
-});
+gulp.task( 'build', ['images', 'copy_templates', 'copy_fonts', 'css', 'js'], function() {
+	gulp.start( 'build2' );
+} );
 
-gulp.task('master:notimages', ['unrev'], function(){
-	gulp.start('build:notimages');
-});
+gulp.task( 'master:notimages', ['unrev'], function() {
+	gulp.start( 'build:notimages' );
+} );
 
-gulp.task('build:notimages', [
-	'copy_templates',
-	'copy_fonts',
-	'css',
-	'js'
-], function(){
-	gulp.start('build2');
-});
+gulp.task( 'build:notimages', ['copy_templates', 'copy_fonts', 'css', 'js'], function() {
+	gulp.start( 'build2' );
+} );
 
-gulp.task('build2', ['rev'], function(){
-	return gulp.start('replace_wp');
-});
+gulp.task( 'build2', ['rev'], function() {
+	return gulp.start( 'replace_wp' );
+} );
 
-gulp.task('cleanbuild', ['clean_all'], function(){
-	gulp.start('master');
-});
+gulp.task( 'cleanbuild', ['clean_all'], function() {
+	gulp.start( 'master' );
+} );
