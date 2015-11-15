9660e8952e1bce3b23ce290fd7e6e895cb2d5f54
Brad Cornes <bradlc41@gmail.com>
2015-11-15 13:36:35 +0000

    Add IE8 compatibility scripts

--- wp-content/themes/default/src/templates/header.php
+++ wp-content/themes/default/src/templates/header.php
@@ -14,5 +14,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
 		<link rel="stylesheet" href="assets/css/main.css">
+
+		<!--[if lt IE 9]>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.3.1/es5-shim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.3.1/es5-sham.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
 	<body>
